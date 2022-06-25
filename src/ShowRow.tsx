894b6b58fca6a73c4d527a877ac5cc0e7c1468c8
Ankit Bisht <eminentman91@gmail.com>
2022-06-25 19:24:57 +0530

    width changed of img

--- src/ShowRow.tsx
+++ src/ShowRow.tsx
@@ -6,8 +6,9 @@ type ShowRowProps = {
 }
 const ShowRow: FC<ShowRowProps> = ({ show }) => {
     return <div className="flex items-stretch rounded-md p-2 bg-gray-700">
-        <img src={show.image?.medium} />
-        <div>
+        <img className="w-36" src={show.image?.medium ||
+            "https://image.shutterstock.com/image-vector/no-image-vector-symbol-missing-260nw-1310632172.jpg"} />
+        <div className="ml-2">
             <h3>{show.name}</h3>
             <p>{show.summary}</p>
         </div>
